c5813e66c1bb0aa50abaff07ff7eda91a281a86f
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2020-08-22 16:09:08 +0200

    WIP: add database abstraction layer

--- main.py
+++ main.py
@@ -4,78 +4,77 @@ import pprint
 
 import json
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from schema import Base, Switch, SwitchModel, Vlan, PortModel, PortType
+import database
 
 def main():
-    print('setting up database...')
+    # Create vlans
+    database.add_vlan(tag = 1005, description = 'internal')
+    database.add_vlan(tag = 107, description = 'guest')
 
-    engine = create_engine('sqlite:///')
-    Base.metadata.create_all(engine)
-    Base.metadata.bin = engine
-
-    sessionm = sessionmaker()
-    sessionm.configure(bind = engine)
-    session = sessionm()
-
-
-    # Parse port types
-    port_type_str = """
-    [
-      { "description": "100BASE-TX", "speed": "100"  },
-      { "description": "1000BASE-T", "speed": "1000" },
-      { "description": "1000BASE-X", "speed": "1000" }
+    # Create port types
+    pts = [
+      { 'description': '100BASE-TX', 'speed': 100  },
+      { 'description': '1000BASE-T', 'speed': 1000 },
+      { 'description': '1000BASE-X', 'speed': 1000 }
     ]
-    """
-    for item in json.loads(port_type_str):
-        port_type = PortType(description = item['description'], speed = item['speed'])
-        session.add(port_type)
-    session.commit()
+    for pt in pts:
+        database.add_port_type(description = pt['description'], speed = pt['speed'])
 
 
-    gigabit_tp = session.query(PortType).filter_by(speed = '1000').first()
-    gigabit_fiber = session.query(PortType).filter_by(speed = '1000').first()
-
-    # Create switch model
-    ps = []
-    for i in range(24):
-        p = PortModel(name = 'TP{}'.format(i + 1),
-                      port_type = gigabit_tp)
-        ps.append(p)
+    # Create switch model1
+    ps = [{ 'name': 'TP{}'.format(i + 1), 'port_type': '1000BASE-T' } for i in range(24)]
     for i in range(4):
-        p = PortModel(name = 'F{}'.format(i + 1),
-                      port_type = gigabit_fiber)
-        ps.append(p)
-    model1 = SwitchModel(name = 'ZyXEL GS1910-24', ports = ps)
-
-    switch1 = Switch(name = 'switch1', location = 5, model = model1)
-    v1337 = Vlan(id = 1337, name = 'internal net')
-    v1338 = Vlan(id = 1338, name = 'guest network')
-    #p1 = Port(name = '1', vlans = [ v1337, v1338 ])
-    #p2 = Port(name = '2', vlans = [ v1338, v1337 ])
-    #p3 = Port(name = '3', vlans = [ v1338 ])
-    #p4 = Port(name = '4', vlans = [ v1338 ])
-    #switch1.ports = [ p1, p2, p3, p4 ]
-
-    #session.add(p1)
-    #session.add(p2)
-    #session.add(p3)
-    #session.add(p4)
-    session.add(switch1)
-
-    session.commit()
+        ps.append({ 'name': 'F{}'.format(i + 1), 'port_type': '1000BASE-X' })
+    database.add_switch_model(name = 'ZyXEL GS1910-24', size = 1, ports = ps)
 
 
+    # Create switch model2
+    ps = [{ 'name': 'TP{}'.format(i + 1), 'port_type': '1000BASE-T' } for i in range(20)]
+    database.add_switch_model(name = 'defect ZyXEL GS1910-24', ports = ps)
+    pprint.PrettyPrinter(indent=4).pprint(database.query_switch_model('defect ZyXEL GS1910-24').jsonify())
+    database.modify_switch_model(name = 'defect ZyXEL GS1910-24', size = 1)
+    pprint.PrettyPrinter(indent=4).pprint(database.query_switch_model('defect ZyXEL GS1910-24').jsonify())
 
-    #pprint.PrettyPrinter(indent=4).pprint(session.query(Switch).all())
-    #pprint.PrettyPrinter(indent=4).pprint(session.query(SwitchModel).all())
 
-    for switch in session.query(Switch).all():
-        print('Switch:')
-        pprint.PrettyPrinter().pprint(switch.jsonify())
-        print()
+    # Create switch
+    ports_str = [
+      { 'name': 'TP1', 'vlans': [ 1005 ] },
+      { 'name': 'TP2', 'vlans': [ 1005 ] },
+      { 'name': 'TP3', 'vlans': [ 107 ] },
+      { 'name': 'TP4', 'vlans': [ 1005, 107 ] },
+      { 'name': 'TP6', 'vlans': [ 107 ] },
+      { 'name': 'F2',  'vlans': [ 107 ] }
+    ]
+    database.add_switch(name = 'switch1', model = 'ZyXEL GS1910-24', port_maps = ports_str)
+
+    for sw in database.query_switches():
+        pprint.PrettyPrinter(indent=4).pprint(sw.jsonify())
+
+
+    # Change switch model
+    database.modify_switch('switch1', model = 'defect ZyXEL GS1910-24')
+
+    for sw in database.query_switches():
+        pprint.PrettyPrinter(indent=4).pprint(sw.jsonify())
+
+
+    # Modify switch ports
+    ports_str = [
+      { 'name': 'TP1', 'vlans': [ ] },
+      { 'name': 'TP2', 'vlans': [ ] },
+      { 'name': 'TP3', 'vlans': [ ] },
+      { 'name': 'TP4', 'vlans': [ ] },
+      { 'name': 'TP6', 'vlans': [ ] },
+      { 'name': 'TP10', 'vlans': [ 1005 ] },
+      { 'name': 'TP11', 'vlans': [ 1005 ] },
+      { 'name': 'TP12', 'vlans': [ 107 ] },
+      { 'name': 'TP13', 'vlans': [ 1005, 107 ] },
+      { 'name': 'TP16', 'vlans': [ 1005 ] }
+    ]
+    database.modify_switch('switch1', port_maps = ports_str)
+
+    for sw in database.query_switches():
+        pprint.PrettyPrinter(indent=4).pprint(sw.jsonify())
 
 if __name__ == '__main__':
     main()
