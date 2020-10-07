43e268a9a456b13444b5eb6a614dab7a1572f14c
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2020-10-07 18:31:17 +0200

    Restructure main()
    Use different main function for each application mode

--- switchmng/__main__.py
+++ switchmng/__main__.py
@@ -5,12 +5,12 @@ from switchmng.schema.base import Base
 from switchmng.database import DatabaseConnection
 from switchmng import routes
 
-def main():
+def main() -> int:    #pragma: no cover
     """
-    Run `switchmng` module directly.
+    Run `switchmng` directly.
 
-    Parse all command line arguments, initialize application then
-    start this application.
+    Parse all command line arguments, decide which mode to run in,
+    then start.
     """
 
     # Parse command line arguments
@@ -19,12 +19,22 @@ def main():
     # Initialize database
     db = DatabaseConnection(config.DB_TYPE, config.DB_PATH, config.DB_VERBOSE, Base)
 
+    # Choose main function depending on mode
     if config.MODE == 'webserver':
-        # Run with builtin flask webserver
-        app = routes.create_app(db)
-        app.run(debug = config.DEBUG, host = config.IP, port = config.PORT)
-    elif config.MODE == 'dump':
+        return webserver_main(db)
+
+    if config.MODE == 'dump':
         raise NotImplementedError('dump is not yet implemented!')
 
+    return 1
+
+def webserver_main(db)-> int:    #pragma: no cover
+    """Main function when running in webserver mode"""
+
+    # Run with builtin flask webserver
+    app = routes.create_app(db)
+    app.run(debug = config.DEBUG, host = config.IP, port = config.PORT)
+    return 0
+
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
